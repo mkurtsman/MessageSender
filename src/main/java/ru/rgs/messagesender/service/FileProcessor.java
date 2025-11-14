dba20d2ffcb7817746032bc10536f1fc7a91979c
MKurtsman <mikhail.kurtsman@bscmsc.ru>
2025-11-14 13:45:21 +0300

    ESB-22595

---
+++ src/main/java/ru/rgs/messagesender/service/FileProcessor.java
@@ -0,0 +1,21 @@
+package ru.rgs.messagesender.service;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Component
+public class FileProcessor implements Processor {
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        Path path = Paths.get("C:\\Users\\MKurtsman\\work\\messagesender\\src\\main\\resources\\body.xml");
+        String body = Files.readString(path);
+
+        exchange.getIn().setBody(body, String.class);
+    }
+}
